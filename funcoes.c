28bd4db23be2b2f74b6051b5948a30c3e30fed73
camilareixs <132021344+camilareixs@users.noreply.github.com>
2023-10-09 20:41:12 -0300

    Adaptando as funções Cadastrar cliente

---
+++ funcoes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funcoes.h"
+
+void cadastrarCliente(clientedesc *Clientes) {
+    if (Clientes -> qtd >= 1000)
+    printf("Você ultrapassou o limite de clientes")
+    return;
+} clientedesc NovoCliente;
+
+printf("Digite o seu nome: ");
+scanf("%c", &NovoCliente.nome)
+
+
+
