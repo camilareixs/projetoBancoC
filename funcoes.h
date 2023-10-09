b6d83b42841f4adc33a196c136388157de91cd46
MMOsbc <115129142+MMOsbc@users.noreply.github.com>
2023-10-09 20:20:37 -0300

    gerando caracteristicas dos clientes

---
+++ funcoes.h
@@ -0,0 +1,24 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+// type def do truck irá informar o que será permitido dentro dele e a quantidade de caracteres
+typedef struct {
+    char nome[50]
+    long cpf;
+    char tipo de conta[5];
+    double valorinicial;
+    int senha;
+
+} 
+clientedesc;
+// define a quantidade maxima de clientes
+typedef struct {
+    Clientes Clientes[1000];
+    int qtd;
+}
+totalclientes
+
+
+
+
+#endif
